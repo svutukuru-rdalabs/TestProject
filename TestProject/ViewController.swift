0fe4e6749ca7d6a3c61ce3ef24a877cea1695009
Vutukuru Suresh <svutukuru@rdalabs.com>
2018-05-10 12:37:30 +0530

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Vutukuru Suresh on 10/05/18.
+//  Copyright © 2018 Vutukuru Suresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
